f63c74c7abce0b60058fbc07dfb6dcda253add1e
Francesco Basco <francesco.basco@msc.com>
2021-03-06 16:20:09 +0100

    finalization

--- src/Actors/SimulationActor.cs
+++ src/Actors/SimulationActor.cs
@@ -89,8 +89,6 @@ namespace Actors
                 raceControl.Tell(new AthleteRegistered(bibId));
             }
 
-            //var standingActor = Context.System.ActorSelection($"/user/standing");
-            //standingActor.Tell(new RaceStarted(raceStartedAt));
             raceControl.Tell(new RaceStarted(raceStartedAt));
 
             for (int i = 0; i < msg.NumberOfAthletes; i++)
@@ -104,11 +102,6 @@ namespace Actors
                 new RaceClosed(),
                 Self);
 
-
-            //Context.System.Scheduler.ScheduleTellOnce(_raceDuration,
-            //    standingActor,
-            //    new PrintFinalStanding(10),
-            //    Self);
 
         }
 
