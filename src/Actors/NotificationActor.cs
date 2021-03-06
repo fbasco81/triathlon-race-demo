6974b00f12106c1c61fadd8c6d54904ab9abf4d3
Francesco Basco <francesco.basco@msc.com>
2021-03-06 17:59:30 +0100

    wip

--- src/Actors/NotificationActor.cs
+++ src/Actors/NotificationActor.cs
@@ -34,12 +34,11 @@ namespace Actors
         {
             var position = 1;
 
+            var propsPhoneNotificationActor = Props.Create<PhoneNotificationActor>();
+            var phoneNotificationActor = Context.ActorOf(propsPhoneNotificationActor, $"phoneNotification");
+
             foreach (var result in msg.Results.Take(30))
             {
-                System.Threading.Thread.Sleep(500);
-                var propsPhoneNotificationActor = Props.Create<PhoneNotificationActor>();
-                var phoneNotificationActor = Context.ActorOf(propsPhoneNotificationActor, $"phoneNotification-{result.BibId}");
-
                 phoneNotificationActor.Tell(
                     new SendPhoneNotification(result.BibId, result.Duration, position)
                 );
